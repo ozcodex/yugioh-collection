46c9e4cf1531458799a889cbe921b2598e8ccf2b
Oscar Bernal <oscarbernal93@gmail.com>
2022-02-07 18:21:50 -0500

    Improve Function cardInfo in experimental offline feature

--- exp.js
+++ exp.js
@@ -1,19 +1,40 @@
 // this is an experiment
 // the idea behind this experiment is to manage the card collection
 // offline and using only node core libraries, without installing anything.
-
-const data = require("./all_cards.json").data;
+const fs = require("fs");
+const all_cards = require("./all_cards.json").data;
 
 function cardInfo(id) {
-  let card = data.filter((card) =>
-    card.card_sets?.some((set) => set.set_code == id)
+  const card = all_cards.filter((card) =>
+    card.card_sets?.some((set) => set.set_code === id)
   )[0];
+  if (!card) return null;
+  const set = card.card_sets.filter(set => set.set_code === id)[0]
   return {
     id: id,
     name: card.name,
+    type: card.type,
+    desc: card.desc,
+    atk: card.atk,
+    def: card.def,
+    level: card.level,
+    race: card.race,
+    attribute: card.attribute,
+    rarity: set?.set_rarity, 
+    set_name: set?.set_name, 
+    price: set?.set_price,
+    image: card.card_images[0].id
   };
+}
+
+function readInputFile(filename) {
+  return fs
+    .readFileSync(filename)
+    .toString()
+    .split("\n")
+    .map((line) => line.replace(/[^ -~]+/g, ""));
 }
 
 console.log(cardInfo("FOTB-EN043"));
 console.log(cardInfo("YS15-ENF24"));
-console.log(data[10])
+console.log(readInputFile('out.txt'))
