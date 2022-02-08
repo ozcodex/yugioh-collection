ca2e957bf1a2f9c773c58515f046f82cd079f53b
Oscar Bernal <oscarbernal93@gmail.com>
2022-02-08 11:34:20 -0500

    Add delete functions and modify db structure

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function cardInfo(id) {
     attribute: card.attribute,
     rarity: set?.set_rarity,
     set_name: set?.set_name,
+    set_id: id.split('-')[0],
     price: averagePrice(card.card_prices[0]),
     price_low: lowestPrice(card.card_prices[0]),
     image: card.card_images[0].id,
@@ -126,30 +127,6 @@ function loadFile(name) {
 //========================//
 
 const args = process.argv.slice(2);
-if (args.length == 0 || args[0] == "-h")
-  console.info(`
-Yugioh Collection Manager
-
-  -l 
-    list all cards in collection.
-
-  -i
-    shows relevant info about current collection
-    
-  -s property value [strict]
-    search the cards containing the value in the property,
-    stricts is a optional boolean and indicates if strict
-    search should be used.
-
-  -a filename
-    adds the ids indicated in the filename to database
-
-  -D
-    Delete all registers in database
-
-  -h
-    shows this help
-`);
 
 switch (args[0]) {
   case "-l":
@@ -161,21 +138,95 @@ switch (args[0]) {
       console.info(`Total value: $${totalValue()}`)
       console.info(`Total low value: $${totalLowValue()}`)
     break;
+  case '-c':
+    card_id = args[1];
+    if (card_id in db) {
+      let card = db[card_id]
+      for(key in card){
+        value = card[key]
+        separator = (key.length >= 8)? '\t':'\t\t'
+        if (value) console.info(`${key}:${separator}${value}`)
+      }
+    }else{
+      console.error(`${card_id} not found`)
+    }
+    break;
   case "-s":
     searchCard(args[1], args[2], args[3] == "true").forEach((card) =>
       console.info(`  ${id}\t (x${card.amount}) ${card.name || ""}`)
     );
     break;
   case "-a":
-    let filename = args[1];
+    filename = args[1];
     if (fs.existsSync(filename)) loadFile(filename);
     else console.error("bad file");
+    break;
+  case '-r':
+    card_id = args[1];
+    if (card_id in db) {
+      if(--db[card_id].amount < 0){
+        delete db[card_id]
+        console.info(`${card_id} deleted from db`)
+      }else{
+        console.info(`${card_id} amount reduced`)
+      }
+      fs.appendFileSync('deleted.txt',card_id)
+      saveDB();
+    }else{
+      console.error(`${card_id} not found`)
+    }
+    break;
+  case '-d':
+    card_id = args[1];
+    if (card_id in db) {
+      delete db[card_id]
+      fs.appendFileSync('deleted.txt',card_id)
+      console.info(`${card_id} deleted from db`)
+      saveDB();
+    }else{
+      console.error(`${card_id} not found`)
+    }
     break;
   case "-D":
     for (id in db) delete db[id];
     saveDB();
     console.info("Done!");
     break;
+  case '-h':
+  console.info(`
+Yugioh Collection Manager
+
+  -l 
+    list all cards in collection.
+
+  -i
+    shows relevant info about current collection
+  
+  -c id
+    shows detailed information about card
+
+  -s property value [strict]
+    search the cards containing the value in the property,
+    stricts is a optional boolean and indicates if strict
+    search should be used.
+
+  -a filename
+    adds the ids indicated in the filename to database
+
+  -r id
+    removes a card from collection decreasing its amount or
+    deleting it if is the last one
+
+  -d id
+    deletes a card using it id
+
+  -D
+    Delete all registers in database
+
+  -h
+    shows this help
+`);
+    break;
   default:
-    console.info(`Invalid option, use -h to for more information`);
+    console.info(`Invalid option '${args[0]}', use -h to for more information`);
 }
